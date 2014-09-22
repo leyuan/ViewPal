f0faa300a04bdfc6798a695c45273d1141c82395
leyuan <452494219@qq.com>
2014-09-22 17:15:43 -0600

    copyright & footer

--- application/views/static/footer.php
+++ application/views/static/footer.php
@@ -2,26 +2,30 @@
             <div class='static-footer'>
                 <div class="container">
                     <div class='upper-footer'>
-                        <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3 text-left">
+                        <div class="footer-col col-xs-12 col-sm-3 col-md-3 col-lg-3 text-left">
                             <h4>About ViewPal</h4>
                                 <a href="">Features</a>
                         </div>
-                        <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3 text-left">
+                        <div class="footer-col col-xs-12 col-sm-3 col-md-3 col-lg-3 text-left">
                             <h4>About ViewPal</h4>
                                 <a href="">Features</a>
                         </div>
-                        <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3 text-left">
+                        <div class="footer-col col-xs-12 col-sm-3 col-md-3 col-lg-3 text-left">
                             <h4>About ViewPal</h4>
                                 <a href="">Features</a>
                         </div>
-                        <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3 text-left">
+                        <div class="footer-col col-xs-12 col-sm-3 col-md-3 col-lg-3 text-left">
                             <h4>About ViewPal</h4>
                                 <a href="">Features</a>
                         </div>                        
                     </div>
                 </div>
                 <div class='lower-footer'>
-                    
+                    <div class="container copyright-container">
+                        <div class="copyright">
+                            <h5>&copy; 2014 ViewPal. All rights reserved. <a href=""> Terms of Use and Privacy Policy </a></h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
