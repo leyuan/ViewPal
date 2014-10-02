252a5ee0dc9a65daf048f1de3bfee5e55301120a
leyuan <452494219@qq.com>
2014-10-01 18:43:36 -0600

    responsive front page

--- application/views/static/body/index.php
+++ application/views/static/body/index.php
@@ -32,15 +32,15 @@
             </div>
             <div class="home-section home-body">
                 <div class="home-text-container">
-                    <h1>Google Analytics tells you what’s happening.</h1>
-                    <h1 class="viewpal-blue">KISSmetrics tells you who’s doing it.</h1>
+                    <h1>You create quality content</h1>
+                    <h1 class="viewpal-blue">Viewpal helps you monetize it</h1>
                     <span class="home-body-btn">
                          <button class="btn home-btn btn-home-try">Try ViewPal Risk Free</button>
                          <button class="btn home-btn btn-home-tour">Take a Tour</button>
                     </span>
                 </div>
                 <div class="home-happy-girl">
-                    <img src="<?php echo public_url()."imgs/happy_man.png";?>" class="happygirl-img-home">
+                    <img src="<?php echo public_url()."imgs/happy_girl.png";?>" class="happygirl-img-home">
                 </div>
                  <div class="home-img-container">
                      <img src="<?php echo public_url()."imgs/home-graph.png"?>" class="graph-img-home">
@@ -50,8 +50,16 @@
                 </div>
             </div>
        </div>
-        <div class="home-section home-footer">
-            <h1> footer here </h1>
+        <div class="home-section home-footer text-center">
+            <ul class="home-footer">
+                <li><a href="<?php echo base_url()."index.php/features"?>">Features</a></li>
+                <li><a href="<?php echo base_url()."index.php/whyviewpal"?>">Why ViewPal</a></li>
+                <li><a href="<?php echo base_url()."index.php/pricing"?>">Pricing</a></li>
+                <li><a href="<?php echo base_url()."index.php/blog"?>">Blog</a></li>
+                <li><a href="<?php echo base_url()."index.php/contact"?>">Contact</a></li>
+                <li><a href="<?php echo base_url()."index.php/career"?>">Career</a></li>
+            </ul>
+            <h4>© 2014 ViewPal · All rights reserved · Privacy Policy · Terms of Use</h4>
         </div>
    </body>
 </html>
